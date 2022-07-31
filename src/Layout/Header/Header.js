b674a2fc1abb5b0f99dd6c6783196e3f8efc4476
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-07-31 10:48:19 +0600

    header work start

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './Header.css';
 
 function Header(props) {
     return (
         <header>
-            Header
+            <a href="#" id="logo">
+                <img src={require('./logo.png')} alt="img" />
+            </a>
         </header>
     );
 }
