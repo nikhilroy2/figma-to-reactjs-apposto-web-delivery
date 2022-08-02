6b79977ff5ebb6fda581cd53aeb674cf19a4933d
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-08-02 16:16:56 +0600

    update

--- src/Components/Section1/Section1.js
+++ src/Components/Section1/Section1.js
@@ -3,7 +3,19 @@ import './Section1.css'
 function Section1(props) {
     return (
         <section id="Section1">
+            <div className="vegemeat_box">
+                <img src={require('./vegemeat.png')} alt="img" />
+                <div className="info_box">
+                    
+                </div>
+            </div>
+            <div className="eggs_box">
+                <img src={require('./eggs.png')} alt="img" />
+            </div>
 
+            <div className="orange_box">
+                <img src={require('./orange.png')} alt="img" />
+            </div>
         </section>
     );
 }
